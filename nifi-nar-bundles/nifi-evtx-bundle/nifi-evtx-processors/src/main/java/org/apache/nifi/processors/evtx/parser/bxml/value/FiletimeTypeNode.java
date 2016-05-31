1e96d17681a20080521f7c8687ae6200caf65b02
Bryan Rosander <brosander@hortonworks.com>
2016-05-31 11:38:27 -0400

    progress

--- nifi-nar-bundles/nifi-evtx-bundle/nifi-evtx-processors/src/main/java/org/apache/nifi/processors/evtx/parser/bxml/value/FiletimeTypeNode.java
+++ nifi-nar-bundles/nifi-evtx-bundle/nifi-evtx-processors/src/main/java/org/apache/nifi/processors/evtx/parser/bxml/value/FiletimeTypeNode.java
@@ -5,17 +5,26 @@ import org.apache.nifi.processors.evtx.parser.bxml.BxmlNode;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * Created by brosander on 5/26/16.
  */
 public class FiletimeTypeNode extends VariantTypeNode {
-    private final Date value;
+    private static final SimpleDateFormat FORMAT = initFormat();
+    private final String value;
+
+    private static final SimpleDateFormat initFormat() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+        return simpleDateFormat;
+    }
 
     public FiletimeTypeNode(InputStream inputStream, long offset, ChunkHeader chunkHeader, BxmlNode parent, int length) throws IOException {
         super(inputStream, offset, chunkHeader, parent, length);
-        value = readFileTime();
+        value = FORMAT.format(readFileTime());
     }
 
     @Override
--- nifi-nar-bundles/nifi-evtx-bundle/nifi-evtx-processors/src/main/java/org/apache/nifi/processors/evtx/parser/bxml/value/SystemtimeTypeNode.java
+++ nifi-nar-bundles/nifi-evtx-bundle/nifi-evtx-processors/src/main/java/org/apache/nifi/processors/evtx/parser/bxml/value/SystemtimeTypeNode.java
@@ -1,24 +1,46 @@
 package org.apache.nifi.processors.evtx.parser.bxml.value;
 
+import com.google.common.primitives.UnsignedInteger;
 import org.apache.nifi.processors.evtx.parser.ChunkHeader;
 import org.apache.nifi.processors.evtx.parser.bxml.BxmlNode;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 /**
  * Created by brosander on 5/26/16.
  */
 public class SystemtimeTypeNode extends VariantTypeNode {
+    private static final SimpleDateFormat FORMAT = initFormat();
+    private final String value;
+
+    private static final SimpleDateFormat initFormat() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+        return simpleDateFormat;
+    }
+
     public SystemtimeTypeNode(InputStream inputStream, long offset, ChunkHeader chunkHeader, BxmlNode parent, int length) throws IOException {
         super(inputStream, offset, chunkHeader, parent, length);
-        for (int i = 0; i < 7; i++) {
-            readWord();
-        }
+        int year = readWord().intValue();
+        int month = readWord().intValue();
+        int dayOfWeek = readWord().intValue();
+        int day = readWord().intValue();
+        int hour = readWord().intValue();
+        int minute = readWord().intValue();
+        int second = readWord().intValue();
+        int microsecond = readWord().intValue();
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, day, hour, minute, second);
+        calendar.set(Calendar.MILLISECOND, microsecond / 1000);
+        value = FORMAT.format(calendar);
     }
 
     @Override
     public String getValue() {
-        return "TODO";
+        return value;
     }
 }
